ce1a6b4285806b37d9079b01e97ad99a6825bcd4
Josh Johnson <josh.johnson@browserlondon.com>
2015-03-20 11:58:23 +0000

    Add twine theme

--- Resources/public/config.js
+++ Resources/public/config.js
@@ -6,5 +6,6 @@
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
+    config.skin = 'twine';
 	// config.uiColor = '#AADC6E';
 };
