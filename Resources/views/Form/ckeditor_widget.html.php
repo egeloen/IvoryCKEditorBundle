f0a512c1d41d695b16aca5846cbe3b4dfdca19d5
Ilya Antipenko <iantipenko@grossum.com>
2015-05-08 10:11:37 +0300

    Wrap widget code in $(document).ready()

--- Resources/views/Form/ckeditor_widget.html.php
+++ Resources/views/Form/ckeditor_widget.html.php
@@ -11,20 +11,28 @@
         <?php endif; ?>
     <?php endif; ?>
     <script type="text/javascript">
-        <?php echo $view['ivory_ckeditor']->renderDestroy($id); ?>
+        <?php if ($jquery) : ?>
+            $(function () {
+        <?php endif; ?>
 
-        <?php foreach ($plugins as $pluginName => $plugin): ?>
-            <?php echo $view['ivory_ckeditor']->renderPlugin($pluginName, $plugin); ?>
-        <?php endforeach; ?>
+            <?php echo $view['ivory_ckeditor']->renderDestroy($id); ?>
 
-        <?php foreach ($styles as $styleName => $style): ?>
-            <?php echo $view['ivory_ckeditor']->renderStylesSet($styleName, $style); ?>
-        <?php endforeach; ?>
+            <?php foreach ($plugins as $pluginName => $plugin): ?>
+                <?php echo $view['ivory_ckeditor']->renderPlugin($pluginName, $plugin); ?>
+            <?php endforeach; ?>
 
-        <?php foreach ($templates as $templateName => $template): ?>
-            <?php echo $view['ivory_ckeditor']->renderTemplate($templateName, $template); ?>
-        <?php endforeach; ?>
+            <?php foreach ($styles as $styleName => $style): ?>
+                <?php echo $view['ivory_ckeditor']->renderStylesSet($styleName, $style); ?>
+            <?php endforeach; ?>
 
-        <?php echo $view['ivory_ckeditor']->renderWidget($id, $config, $inline, $input_sync); ?>
+            <?php foreach ($templates as $templateName => $template): ?>
+                <?php echo $view['ivory_ckeditor']->renderTemplate($templateName, $template); ?>
+            <?php endforeach; ?>
+
+            <?php echo $view['ivory_ckeditor']->renderWidget($id, $config, $inline, $input_sync); ?>
+
+        <?php if ($jquery) : ?>
+            });
+        <?php endif; ?>
     </script>
 <?php endif; ?>
