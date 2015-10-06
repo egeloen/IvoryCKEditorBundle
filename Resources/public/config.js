45b81fd133aa1d3ce6ec13f6149f40c2bd05bb57
Josh Johnson <josh.johnson@browserlondon.com>
2015-10-06 17:59:40 +0100

    Theme build

--- Resources/public/config.js
+++ Resources/public/config.js
@@ -7,7 +7,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
     config.skin = 'twine-v2';
-    config.removePlugins = 'elementspath', 'resize';
+    config.removePlugins = 'elementspath, resize';
     config.resize_enabled = false;
 	// config.uiColor = '#AADC6E';
 };
